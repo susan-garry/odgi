39c6f45d90871c260103449c2c7a03a724a0b48d
susan-garry <sgarry406@gmail.com>
2024-07-01 16:36:07 -0400

    Add a simple benchmarking test to traverse through all steps in the graph, in path order

--- src/subcommand/depth_opts_main.cpp
+++ src/subcommand/depth_opts_main.cpp
@@ -33,12 +33,19 @@ namespace odgi {
                                                   "The file must contain one path name per line and a subset of all paths can be specified; "
                                                   "If a step is of a path of the given list, it is taken into account when calculating a node's depth. Else not.",
                                                   {'s', "subset-paths"});
+        args::ValueFlag<uint64_t> random(depth_opts, "N",
+                          "Print a randomly generated, newline-separated subset of paths specified in the FILE. "
+                          "The percentage of paths selected is determined by N. Duplicates are permitted.",
+                          {'r', "random-subset"});
         args::Flag graph_depth_vec(depth_opts, "graph-depth-vec",
                                    "Compute the depth on each node in the graph, writing a vector by base in one line.",
                                    {'v', "graph-depth-vec"}, "boolean");
         args::ValueFlag<std::string> _optimizations(depth_opts, "optimizations",
-                                                  "The optimization function to be benchmarked",
-                                                  {'O', "depth-opt"}, "baseline");
+                                                    "The optimization function to be benchmarked.",
+                                                    {'O', "depth-opt"}, "baseline");
+        // args::Flag<std::string> opt_subset_paths(depth_opts, "subset_paths_optimization",
+        //                                                    "Flag to determine whether or not subset_paths is computed in parallel.",
+        //                                                    {'S', "opt-subset-paths"});
 
         args::Group threading_opts(parser, "[ Threading ] ");
         args::ValueFlag<uint64_t> _num_threads(threading_opts, "N", "Number of threads to use in parallel operations.", {'t', "threads"});
@@ -86,6 +93,9 @@ namespace odgi {
             exit(1);
         }
 
+        // time how long it takes to execute
+        auto t1 = std::chrono::high_resolution_clock::now();
+
         // Compute paths_to_consider
         const std::string optimizations = args::get(_optimizations);
         std::vector<bool> paths_to_consider;
@@ -94,24 +104,21 @@ namespace odgi {
 
             std::ifstream refs(args::get(_subset_paths).c_str());
             std::string line;
-            if (optimizations == "baseline") {
-                while (std::getline(refs, line)) {
-                    if (!line.empty()) {
-                        if (!graph.has_path(line)) {
-                            std::cerr << "[odgi::depth-opts] error: path " << line << " not found in graph" << std::endl;
-                            exit(1);
-                        }
 
-                        paths_to_consider[as_integer(graph.get_path_handle(line))] = true;
+            while (std::getline(refs, line)) {
+                if (!line.empty()) {
+                    if (!graph.has_path(line)) {
+                        std::cerr << "[odgi::depth-opts] error: path " << line << " not found in graph" << std::endl;
+                        exit(1);
                     }
+                    paths_to_consider[as_integer(graph.get_path_handle(line))] = true;
                 }
-            } else {
-                std::cerr << "[odgi::depth-opts] error: optimization " << optimizations << " not recognized" << std::endl;
-                exit(1);
             }
         } else {
             paths_to_consider.resize(graph.get_path_count() + 1, true);
         }
+
+        auto t2 = std::chrono::high_resolution_clock::now();
 
         // Compute depths
         auto get_graph_node_depth = [](const odgi::graph_t &graph, const handle_t h,
@@ -137,19 +144,47 @@ namespace odgi {
                 [&](const handle_t& h) {
                     auto id = graph.get_id(h);
                     depths[id - shift] = get_graph_node_depth(graph, h, paths_to_consider);
-                }, true);
+                }, false);
         } else {
-            std::cerr << "[odgi::depth-opts] error: optimizations " << optimizations << " not recognized" << std::endl;
-            exit(1);
+            graph.for_each_handle(
+                [&](const handle_t& h) {
+                    auto id = graph.get_id(h);
+                    depths[id - shift] = get_graph_node_depth(graph, h, paths_to_consider);
+                }, true);
         }
+
+        auto t3 = std::chrono::high_resolution_clock::now();
 
         // Print output to commandline if requested
         if (graph_depth_vec) {
             std::cout << (og_file ? args::get(og_file) : "graph") << "_vec";
+            // int max_depth = 0;
             for (uint64_t i = 0; i < graph.get_node_count(); ++i) {
                 std::cout << " " << depths[i];
+                // if (depths[i] > max_depth) {
+                //     max_depth = depths[i];
+                // }
             }
+            // std::cout << "max depth: " << max_depth;
             std::cout << std::endl;
+        } else if (random) { // If specified, print a random select of the paths in our input graph
+            const uint64_t p = graph.get_path_count();
+            const uint64_t n = args::get(random) * p/100;
+            std::vector<path_handle_t> path_handles(p);
+            int i = 0;
+            graph.for_each_path_handle([&](const path_handle_t &path_h) {
+                path_handles[i] = path_h;
+                i++;
+            });
+            for (uint64_t i = 0; i < n; i++) {
+                std::cout << graph.get_path_name(path_handles[std::rand() % p]) << std::endl;
+            }
+        } else { // Print how long it takes the node depth computation to execute. subset\tdepth\ttotal
+            // Get time in milliseconds as a double
+            std::chrono::duration<double, std::milli> subset_time = t2 - t1;
+            std::chrono::duration<double, std::milli> depths_time = t3 - t2;
+            std::chrono::duration<double, std::milli> total_time = t3 - t1;
+            std::cout << subset_time.count() << "\t" << depths_time.count() << "\t" << total_time.count() << std::endl;
         }
 
         return 0;
